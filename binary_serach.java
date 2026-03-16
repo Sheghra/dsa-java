5bb732ce2aa478d8273ff25c4a22bad1d6f0af63
Abhishek <abhishek13534@gmail.com>
2026-03-17 01:34:20 +0530

    hehe

---
+++ binary_serach.java
@@ -0,0 +1,23 @@
+public class binary_serach{
+    public static void main(String[] args){
+        int arr[]={1,2,34,54,3,6,24,65};
+        int index = binsearch(arr,52);
+
+        if(!(index==-1))
+        {
+            System.out.println("The value is at index : "+index);
+        }
+        else
+            System.out.println("Element not present in the list");
+    }
+
+    public static int binsearch(int[] a,int value){
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==value){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
